dbfb9bdd9fb5a7ecaa43e5498ce1af8b49f11167
Albert3059 <48489233+Albert3059@users.noreply.github.com>
2025-09-17 02:02:26 +0200

    ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// layout.tsx or layout.jsx
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
@@ -8,7 +9,10 @@ export const metadata: Metadata = {
   title: 'Digilink IT Solutions',
   description: 'Providing IT services, cloud solutions, and digital support for your business.',
   generator: 'Digilink IT Solutions',
-  
+  icons: {
+    icon: '/favicon.ico', // <-- Path to your icon file in /public folder
+   
+  },
 }
 
 export default function RootLayout({
